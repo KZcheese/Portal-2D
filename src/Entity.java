2ae04b8c5ea886bd948fb0985f6622e3a095c9cb
Kevin Zhan <kevin.w.zhan@gmail.com>
2015-05-28 11:13:35 -0400

    Added timscale and stuff

--- src/Entity.java
+++ src/Entity.java
@@ -1,8 +1,9 @@
 import java.awt.geom.Point2D;
 
 public abstract class Entity {
-	Point2D position;
-	SpriteSheet sSheet;
+	private Point2D position;
+	private SpriteSheet sSheet;
+	private double timeScale;
 
 	public void move(double x, double y) {
 		position.setLocation(position.getX() + x, position.getY() + y);
