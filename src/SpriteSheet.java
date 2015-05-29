a27b6b28dcb78e7ff46a26897758118d73c93f3a
Nebuli2 <b.w.hetherington@gmail.com>
2015-05-29 10:54:17 -0400

    SpriteSheet fix

--- src/SpriteSheet.java
+++ src/SpriteSheet.java
@@ -1,4 +1,69 @@
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
 
 public class SpriteSheet {
-
+	public static class Animation {
+		private int start, stop, current;
+		
+		/**
+		 * Constructs an animation with a specified start and stop
+		 * frame.
+		 * @param start The first frame of the animation
+		 * @param stop The last frame of the animation
+		 */
+		public Animation(int start, int stop) {
+			this.start = start;
+			this.stop = stop;
+			current = start;
+		}
+		
+		/**
+		 * Returns the current frame.
+		 * @return The current frame
+		 */
+		public int getCurrent() {
+			return current;
+		}
+		
+		/**
+		 * Returns true if the current frame is less than the end frame.
+		 * @return Whether or not the animation is complete
+		 */
+		public boolean isDone() {
+			return current < stop;
+		}
+		
+		/**
+		 * Increments the current frame if the animation is not complete.
+		 */
+		public void update() {
+			if (!isDone()) {
+				current++;
+			}
+		}
+	}
+	
+	private BufferedImage image;
+	private Animation currentAnimation;
+	private int width, height, frames;
+	
+	public SpriteSheet(BufferedImage image, int width, int height, int frames) {
+		this.image = image;
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Image getCurrentFrame() {
+		int rows = image.getWidth() / width,
+			cols = image.getHeight() / height,
+			row = currentAnimation.getCurrent() % rows,
+			col = currentAnimation.getCurrent() / cols;
+		return image.getSubimage(row * width, col * height, width, height);
+	}
+	
+	public void update() {
+		currentAnimation.update();
+	}
+	
 }
