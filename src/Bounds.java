d9d832f153b69c92f51769c7762f7ded2d3b679c
Nebuli2 <b.w.hetherington@gmail.com>
2015-05-29 11:40:07 -0400

    Bounds fix

--- src/Bounds.java
+++ src/Bounds.java
@@ -5,7 +5,7 @@ import java.awt.geom.Area;
 import java.awt.geom.Point2D;
 
 public class Bounds {
-	private DPoint center;
+	private Point2D center;
 	private Area area;
 	private double angle;
 	
@@ -15,19 +15,19 @@ public class Bounds {
 			area.add(new Area(shape));
 		}
 		Rectangle r = area.getBounds();
-		center = new DPoint(r.getCenterX(), r.getCenterY());
+		center = new Point2D.Double(r.getCenterX(), r.getCenterY());
 	}
 	
-	public DPoint getCenter() {
+	public Point2D getCenter() {
 		return center;
 	}
 	
 	public double getX() {
-		return center.x;
+		return center.getX();
 	}
 	
 	public double getY() {
-		return center.y;
+		return center.getY();
 	}
 	
 	public boolean intersects(Bounds b) {
@@ -36,26 +36,45 @@ public class Bounds {
 		return !intersection.isEmpty();
 	}
 	
-	public void setCenter(DPoint p) {
-		double dx = p.x - center.x, dy = p.y - center.y;
-		center.translate(dx, dy);
+	public void setCenter(Point2D p) {
+		double dx = p.getX() - center.getX(), dy = p.getY() - center.getY();
+		center.setLocation(center.getX() + dx, center.getY() + dy);
 		area.transform(AffineTransform.getTranslateInstance(dx, dy));
 	}
 	
+	/**
+	 * Rotates the bounds to face the specified angle.
+	 * @param angle The angle of the bounds
+	 */
 	public void setAngle(double angle) {
-		area.transform(AffineTransform.getRotateInstance(angle - this.angle, center.x, center.y));
+		area.transform(AffineTransform.getRotateInstance(angle - this.angle, center.getX(), center.getY()));
 		this.angle = angle;
 	}
 	
+	/**
+	 * Returns the underlying area of the bounds.
+	 * @return The area of the bounds
+	 */
 	public Area getArea() {
 		return area;
 	}
 	
-	public void offset(double x, double y) {
-		area.transform(AffineTransform.getTranslateInstance(x, y));
+	/**
+	 * Translates the bounds a specified 
+	 * @param dx The X distance to translate
+	 * @param dy The Y distance to translate
+	 */
+	public void translate(double dx, double dy) {
+		area.transform(AffineTransform.getTranslateInstance(dx, dy));
 	}
 	
-	public boolean contains(DPoint point) {
-		return area.contains(new Point2D.Double(point.x, point.y));
+	/**
+	 * Returns true if the bounds contains the specified point, and
+	 * false otherwise.
+	 * @param point The point to check
+	 * @return Whether or not the bounds contains the point
+	 */
+	public boolean contains(Point2D point) {
+		return area.contains(point);
 	}
 }
