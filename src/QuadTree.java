6251b22fcbd4a4e8cf700ff42286d51aac8cc326
Kevin Zhan <kevin.w.zhan@gmail.com>
2015-06-01 11:42:21 -0400

    Started QuadTree

--- src/QuadTree.java
+++ src/QuadTree.java
@@ -3,7 +3,18 @@ import java.util.LinkedList;
 
 public class QuadTree {
 	LinkedList<Entity> iterate = new LinkedList<Entity>();
-	Node head = new Node(new Point2D.Double(0, 0));
+	Node head;
+
+	public QuadTree(Level level) {
+		double x = level.getBounds().getCenterX();
+		double y = level.getBounds().getCenterY();
+		head = new Node(new Point2D.Double(x, y));
+
+	}
+
+	public void add(Entity e) {
+		Node node = new Node(e);
+	}
 
 	private class Node {
 		Entity value;
@@ -12,6 +23,7 @@ public class QuadTree {
 
 		public Node(Entity value) {
 			this.value = value;
+			p = value.getPosition();
 		}
 
 		public Node(Point2D p) {
