acc96d27c62f7e8dcb9eac0897c9642cb890eed1
Kevin Zhan <kevin.w.zhan@gmail.com>
2015-06-02 11:26:34 -0400

    derped with quadtree

--- src/QuadTree.java
+++ src/QuadTree.java
@@ -2,18 +2,41 @@ import java.awt.geom.Point2D;
 import java.util.LinkedList;
 
 public class QuadTree {
-	LinkedList<Entity> iterate = new LinkedList<Entity>();
+	LinkedList<Node> iterate = new LinkedList<Node>();
 	Node head;
 
 	public QuadTree(Level level) {
-		double x = level.getBounds().getCenterX();
-		double y = level.getBounds().getCenterY();
-		head = new Node(new Point2D.Double(x, y));
+		double centerX = level.getBounds().getCenterX();
+		double centerY = level.getBounds().getCenterY();
+		head = new Node(new Point2D.Double(centerX, centerY));
+	}
 
+	public void update() {
+		for (Node n : iterate)
+			n.p = n.value.getPosition();
 	}
 
 	public void add(Entity e) {
 		Node node = new Node(e);
+		Node otherNode = head;
+		int nextIndex = -1;
+		while (nextIndex < 0 || otherNode.next[nextIndex] != null) {
+			nextIndex = compare(otherNode, node);
+		}
+		otherNode.next[nextIndex] = node;
+	}
+
+	private int compare(Node a, Node b) {
+		double aX = a.p.getX(), aY = a.p.getX(), bX = b.p.getX(), bY = b.p
+				.getX();
+		if (bX > aX) {
+			if (bY > aY)
+				return 0;
+			return 3;
+		}
+		if (bY > aY)
+			return 1;
+		return 2;
 	}
 
 	private class Node {
