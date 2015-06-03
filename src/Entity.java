71cd673747115b40592043927b87e3ecd4104a80
Kevin Zhan <kevin.w.zhan@gmail.com>
2015-06-03 11:26:27 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/Bounds.java
    	src/Level.java

--- src/Entity.java
+++ src/Entity.java
@@ -4,64 +4,63 @@ import java.awt.Point;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
-
 public class Entity {
 	private Point2D location;
 	private Rectangle2D bounds;
 	private Level level;
 	private boolean usePhysics;
-	
+
 	private double moveAccel, gravityAccel, speed, speedCurrent, angle;
-	
+
 	public Entity(Rectangle2D bounds) {
 		this.bounds = bounds;
 		location = new Point2D.Double();
 		speed = 1;
 	}
-	
+
 	public Entity() {
 		this(null);
 	}
-	
+
 	public void setLocation(Point2D location) {
 		this.location = location;
 	}
-	
+
 	public Point2D getLocation() {
 		return location;
 	}
-	
+
 	public void setLevel(Level l) {
 		level = l;
 	}
-	
+
 	public boolean intersects(Entity e) {
 		return bounds.intersects(e.bounds);
 	}
-	
+
 	public Rectangle2D getBounds() {
 		return bounds;
 	}
-	
+
 	public void updateEntity() {
 		if (level != null) {
 			update();
 		}
 	}
-	
+
 	public void update() {
 		updateLocation();
 	}
-	
+
 	public void updateLocation() {
 		double dx = 0, dy = 0;
-		
+
 		// Gravity
 		if (usePhysics) {
 			dy -= gravityAccel;
 			gravityAccel += 0.1;
 		}
-		
+
 		// Movement
 		speedCurrent += moveAccel;
 		if (speedCurrent > speed) {
@@ -71,48 +70,50 @@ public class Entity {
 			speedCurrent = 0;
 		}
 		moveAccel -= 0.1;
-		
+
 		dx += Math.cos(angle) * speedCurrent;
 		dy += Math.sin(angle) * speedCurrent;
-		
+
 		move(dx, dy);
 	}
-	
+
 	private void move(double dx, double dy) {
 		location.setLocation(location.getX() + dx, location.getY() + dy);
-		bounds.setFrame(location.getX(), location.getY(), bounds.getWidth(), bounds.getHeight());
+		bounds.setFrame(location.getX(), location.getY(), bounds.getWidth(),
+				bounds.getHeight());
 	}
-	
+
 	public void resetGravity() {
 		gravityAccel = 0;
 	}
-	
+
 	public static void movePoint(Point2D p, double dist, double angle) {
 		double dx = Math.cos(angle) * dist, dy = Math.sin(angle) * dist;
 		p.setLocation(p.getX() + dx, p.getY() + dy);
 	}
-	
+
 	public void render(Graphics g) {
 		Point corner = level.getCorner();
-		int dx = (int) (location.getX() - corner.x), dy = (int) (location.getY() - corner.y);
+		int dx = (int) (location.getX() - corner.x), dy = (int) (location
+				.getY() - corner.y);
 		g.translate(dx, dy);
 		((Graphics2D) g).fill(bounds);
 		g.translate(-dx, -dy);
 	}
-	
+
 	public void pushForward() {
 		moveAccel += 1;
 	}
-	
+
 	public void pushUp() {
 		gravityAccel -= 10;
 	}
-	
+
 	public boolean physicsEnabled() {
 		return usePhysics;
 	}
-	
+
 	public void enablePhysics(boolean physics) {
 		usePhysics = physics;
 	}
-} 
+}
--- src/Level.java
+++ src/Level.java
@@ -1,134 +1,31 @@
-<<<<<<< HEAD
-=======
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.geom.Area;
->>>>>>> origin/master
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
-import java.util.LinkedList;
 import java.util.List;
 
-<<<<<<< HEAD
 public class Level {
 	private List<Entity> entities;
 
 	public void update() {
-		for (Entity e : entity) {
+		for (Entity e : entities) {
 			Point2D previous = e.getLocation();
 			e.updateEntity();
 			Point2D location = e.getLocation();
-			if (e.enablePhysics()) {
+			if (e.physicsEnabled()) {
 				Rectangle2D bounds1 = e.getBounds();
 				Rectangle2D bounds2 = e.getBounds();
 			}
 		}
-=======
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.Timer;
-
-
-public class Level {
-	private List<Entity> entities;
-	private Bounds bounds;
-	private Point corner;
-	
-	public Level() {
-		this(new Rectangle());
-	}
-	
-	public Level(Rectangle bounds) {
-		entities = new LinkedList<>();
-		
-		Area a = new Area(new Rectangle(bounds.x - 10, bounds.y - 10, bounds.width + 20, bounds.height + 20));
-		a.subtract(new Area(bounds));
-		
-		this.bounds = new Bounds(a);
-		
-		corner = new Point();
->>>>>>> origin/master
 	}
 
 	public void addEntity(Entity e) {
 		entities.add(e);
-		e.setLevel(this);
 	}
 
 	public void removeEntity(Entity e) {
 		entities.remove(e);
-		e.setLevel(null);
 	}
 
 	public List<Entity> getEntities() {
 		return entities;
-	}
-<<<<<<< HEAD
-
-	public Rectangle2D getBounds() {
-=======
-	
-	public Point getCorner() {
-		return corner;
-	}
-	
-	public Bounds getBounds() {
->>>>>>> origin/master
-		return bounds;
-	}
-	
-	public void render(Graphics g) {
-		for (Entity e : entities) {
-			e.render(g);
-		}
-		((Graphics2D) g).draw(bounds.getArea());
-	}
-	
-	public void update() {
-		for (Entity e : entities) {
-			e.update();
-		}
-	}
-	
-	public static void main(String[] args) {
-		Level l = new Level(new Rectangle(600, 400));
-		class Renderer extends JComponent {
-			
-			public final Level level;
-			
-			public Renderer(Level level) {
-				this.level = level;
-				
-				Timer timer = new Timer(20, new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						Renderer.this.level.update();
-					}
-				});
-				timer.start();
-				Renderer.this.repaint();
-			}
-			
-			public void paintComponent(Graphics g) {
-				level.render(g);
-			}
-			
-		}
-		Renderer r = new Renderer(l);
-		r.setPreferredSize(new Dimension(600, 400));
-		Entity e = new Entity(new Rectangle2D.Double(0, 0, 20, 30));
-		l.addEntity(e);
-		e.setLocation(new Point2D.Double(100, 50));
-		e.pushForward();
-		
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().add(r);
-		frame.pack();
-		frame.setVisible(true);
 	}
 }
