c46e4d4191d82da848a7fc322d65fb5612757b9f
Kevin Zhan <kevin.w.zhan@gmail.com>
2015-06-03 11:41:02 -0400

    updated level

--- src/Level.java
+++ src/Level.java
@@ -1,9 +1,11 @@
+import java.awt.Point;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.util.List;
 
 public class Level {
 	private List<Entity> entities;
+	private Point corner;
 
 	public void update() {
 		for (Entity e : entities) {
@@ -12,9 +14,21 @@ public class Level {
 			Point2D location = e.getLocation();
 			if (e.physicsEnabled()) {
 				Rectangle2D bounds1 = e.getBounds();
-				Rectangle2D bounds2 = e.getBounds();
+				for (Entity e2 : entities) {
+					if (e2 != e) {
+						Rectangle2D bounds2 = e2.getBounds();
+						Rectangle2D collision = null;
+						if (bounds1.intersects(bounds2)) {
+
+						}
+					}
+				}
 			}
 		}
+	}
+
+	public Point getCorner() {
+		return corner;
 	}
 
 	public void addEntity(Entity e) {
@@ -28,4 +42,5 @@ public class Level {
 	public List<Entity> getEntities() {
 		return entities;
 	}
+
 }
