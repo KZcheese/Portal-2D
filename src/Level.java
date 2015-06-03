bb2bd4221c7865c5c1836c1c2aad063e985e76b6
Kevin Zhan <kevin.w.zhan@gmail.com>
2015-06-03 11:22:14 -0400

    Update Level and deleted Bounds

--- src/Level.java
+++ src/Level.java
@@ -1,30 +1,35 @@
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.util.List;
 
-
 public class Level {
 	private List<Entity> entities;
-	private Rectangle2D bounds;
-	
-	public Level(Rectangle2D bounds) {
-		this.bounds = bounds;
+
+	public void update() {
+		for (Entity e : entity) {
+			Point2D previous = e.getLocation();
+			e.updateEntity();
+			Point2D location = e.getLocation();
+			if (e.enablePhysics()) {
+				Rectangle2D bounds1 = e.getBounds();
+				Rectangle2D bounds2 = e.getBounds();
+			}
+		}
 	}
-	
+
 	public void addEntity(Entity e) {
 		entities.add(e);
 	}
-	
+
 	public void removeEntity(Entity e) {
 		entities.remove(e);
 	}
-	
+
 	public List<Entity> getEntities() {
 		return entities;
 	}
-	
+
 	public Rectangle2D getBounds() {
 		return bounds;
 	}
 }
-
-
