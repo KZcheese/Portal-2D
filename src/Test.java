b80eecb19e0334cf4964276cdfbe5076a9986f13
Nebuli2 <b.w.hetherington@gmail.com>
2015-06-04 11:33:57 -0400

    Syncing

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,47 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Rectangle2D;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
 
 public class Test {
-
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Test");
+		class Renderer extends JComponent {
+			public Level level;
+			
+			public Renderer(Level level) {
+				this.level = level;
+			}
+			
+			public void paintComponent(Graphics g) {
+				level.render(g);
+			}
+		}
+		
+		final Level l = new Level();
+		final Renderer r = new Renderer(l);
+		
+		Timer t = new Timer(16, new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				l.update();
+				r.repaint();
+			}
+		});
+		
+		r.setPreferredSize(new Dimension(600, 400));
+		frame.getContentPane().add(r);
+		
+		t.start();
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
 }
