45bc1a5f651264392dfc861cd0eaddc9146d1e67
Kevin Zhan <kevin.w.zhan@gmail.com>
2015-06-04 11:16:00 -0400

    Updated Level

--- src/Level.java
+++ src/Level.java
@@ -1,3 +1,4 @@
+import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
@@ -19,11 +20,52 @@ public class Level {
 						Rectangle2D bounds2 = e2.getBounds();
 						Rectangle2D collision = null;
 						if (bounds1.intersects(bounds2)) {
+							{
+								// Calculate the vertical and horizontal
+								// length between the centres of rectangles
 
+								double hd = Math
+										.abs((bounds1.getCenterX() * bounds1
+												.getCenterX())
+												+ (bounds2.getCenterX() * bounds2
+														.getCenterX()));
+								double vd = Math
+										.abs((bounds1.getCenterY() * bounds1
+												.getCenterY())
+												+ (bounds2.getCenterY() * bounds2
+														.getCenterY()));
+
+								// Now compare them to know the side of
+								// collision
+
+								if (hd < vd) {
+									if (bounds1.getCenterX() < bounds2
+											.getCenterX())
+										;
+									// Collision on right side of player
+									else
+										;
+									// Collision on left side of player
+								} else if (vd < hd) {
+									if (bounds1.getCenterY() < bounds2
+											.getCenterY())
+										;
+									// Collision on bottom side of player
+									else
+										;
+									// Collision on top side of player
+								}
+							}
 						}
 					}
 				}
 			}
+		}
+	}
+
+	public void render(Graphics g) {
+		for (Entity e : entities) {
+			e.render(g);
 		}
 	}
 
