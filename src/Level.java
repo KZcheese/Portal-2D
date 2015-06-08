23dbfd2aae9eb0819374bab4768342c04f6aeaa9
Kevin Zhan <kevin.w.zhan@gmail.com>
2015-06-08 11:31:51 -0400

    Revert "Cleanup"
    This reverts commit e4b24246b272c29e87e737234759d4d4e350ea44.

--- src/Level.java
+++ src/Level.java
@@ -2,6 +2,7 @@ import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Double;
 import java.util.LinkedList;
 import java.util.List;
 
