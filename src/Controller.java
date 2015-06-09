5bf63e9bc0b7bcebf99bbbd6c91080a5dcf34df5
Kevin Zhan <kevin.w.zhan@gmail.com>
2015-06-09 16:34:34 -0400

    Lots of stuff

--- src/Controller.java
+++ src/Controller.java
@@ -1,24 +1,20 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-
 public class Controller implements KeyListener {
 	private Entity unit;
 	private boolean[] keysDown;
 	private boolean jumped;
-	
-	private static final int
-		LEFT = 0,
-		RIGHT = 1,
-		UP = 2;
-	
+
+	private static final int LEFT = 0, RIGHT = 1, UP = 2;
+
 	public Controller(Entity unit) {
 		this.unit = unit;
 		keysDown = new boolean[3];
 	}
-	
+
 	public void keyPressed(KeyEvent e) {
-		
+
 		switch (e.getKeyCode()) {
 		case KeyEvent.VK_A:
 			keysDown[LEFT] = true;
@@ -47,8 +43,9 @@ public class Controller implements KeyListener {
 		}
 	}
 
-	public void keyTyped(KeyEvent e) {}
-	
+	public void keyTyped(KeyEvent e) {
+	}
+
 	public void update() {
 		if (keysDown[UP] && !jumped) {
 			unit.pushUp();
