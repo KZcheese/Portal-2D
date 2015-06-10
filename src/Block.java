beb1c832c4f6049c6b5422f569973795af71b0b5
Nebuli2 <b.w.hetherington@gmail.com>
2015-06-09 23:20:27 -0400

    Basic block system

--- src/Block.java
+++ src/Block.java
@@ -1,3 +1,15 @@
-public abstract class Block extends Entity {
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
+public class Block extends Entity {
+	public static final int SIZE = 64;
+	
+	public Block(int x, int y) {
+		super(new Rectangle2D.Double(x * SIZE, y * SIZE, SIZE, SIZE));
+		enablePhysics(false);
+	}
+	
+	public Block() {
+		this(0, 0);
+	}
 }
--- src/Test.java
+++ src/Test.java
@@ -26,7 +26,7 @@ public class Test {
 			}
 		}
 
-		final Level l = new Level(new Rectangle2D.Double(0, 0, 640, 480));
+		final Level l = new Level(new Rectangle2D.Double(0, 0, 16 * Block.SIZE, 12 * Block.SIZE));
 		final Renderer r = new Renderer(l);
 
 		final Entity entity = new Entity(new Rectangle2D.Double(25, 10, 20, 35));
@@ -35,34 +35,11 @@ public class Test {
 		final Controller c = new Controller(entity);
 		frame.addKeyListener(c);
 
-		Entity wall1 = new Entity(new Rectangle2D.Double(0, 460, 100, 2));
-		wall1.enablePhysics(false);
-		l.addEntity(wall1);
-		
-		Entity wall2 = new Entity(new Rectangle2D.Double(80, 380, 100, 2));
-		wall2.enablePhysics(false);
-		l.addEntity(wall2);
-		
-		Entity wall3 = new Entity(new Rectangle2D.Double(160, 300, 100, 2));
-		wall3.enablePhysics(false);
-		l.addEntity(wall3);
-		
-		Entity wall4 = new Entity(new Rectangle2D.Double(240, 220, 100, 2));
-		wall4.enablePhysics(false);
-		l.addEntity(wall4);
-		
-		Entity wall5 = new Entity(new Rectangle2D.Double(80, 380, 2, 100));
-		wall5.enablePhysics(false);
-//		l.addEntity(wall5);
-		
-		Entity wall6 = new Entity(new Rectangle2D.Double(160, 300, 2, 100));
-		wall6.enablePhysics(false);
-		l.addEntity(wall6);
-		
-		Entity wall7 = new Entity(new Rectangle2D.Double(240, 220, 2, 100));
-		wall7.enablePhysics(false);
-		l.addEntity(wall7);
-	
+		l.addEntity(new Block(0, 11));
+		l.addEntity(new Block(2, 10));
+		l.addEntity(new Block(4, 9));
+		final Block b = new Block(4, 1);
+		l.addEntity(b);
 
 		final int[] a = { 0 };
 		Timer t = new Timer(16, new ActionListener() {
@@ -70,14 +47,14 @@ public class Test {
 				l.update();
 				r.repaint();
 				c.update();
-				wall3.jump();
+				b.jump();
 				// System.out.println(entity.getLocation());
 				a[0]++;
 			}
 		});
 
 		// frame.setPreferredSize(new Dimension(640, 480));
-		r.setPreferredSize(new Dimension(640, 480));
+		r.setPreferredSize(new Dimension(16 * Block.SIZE, 12 * Block.SIZE));
 		frame.getContentPane().add(r);
 
 		t.start();
