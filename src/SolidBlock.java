b6c41d1f38f83992dd11afaf81395c9f9ab414bf
Nebuli2 <b.w.hetherington@gmail.com>
2015-06-10 11:42:58 -0400

    hujkulhgjk

--- src/SolidBlock.java
+++ src/SolidBlock.java
@@ -1,4 +1,22 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
 
 public class SolidBlock extends Block {
-
+	public SolidBlock(int x, int y) {
+		super(x, y);
+	}
+	
+	public void render(Graphics g) {
+		Point corner = level.getCorner();
+		int dx = (int) (getLocation().getX() - corner.x), dy = (int) (location
+				.getY() - corner.y),
+			w = (int) getBounds().getWidth(),
+			h = (int) getBounds().getHeight();
+		g.setColor(new Color(225, 225, 225));
+		g.fillRect(dx, dy, w, h);
+		g.setColor(new Color(235, 235, 235));
+		g.drawRect(dx, dy, w, h);
+	}
 }
