0a4dbfbbe716116d26bfb22e79f127da13755c61
Kevin Zhan <kevin.w.zhan@gmail.com>
2015-06-11 11:42:32 -0400

    Stuff

--- src/PortalGun.java
+++ src/PortalGun.java
@@ -1,4 +1,11 @@
+import java.awt.geom.Rectangle2D;
+
 
 public class PortalGun extends Weapon {
+
+	public PortalGun(Rectangle2D bounds, SpriteSheet sprite) {
+		super(bounds, sprite);
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- src/Weapon.java
+++ src/Weapon.java
@@ -1,3 +1,9 @@
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
 public abstract class Weapon extends Entity {
 
+	public Weapon(Rectangle2D bounds, SpriteSheet sprite) {
+		super(bounds, sprite);
+	}
 }
