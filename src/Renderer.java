b9d1900ddc064f7fe8d9cccbf3fabe1230d1bf1f
Kevin Zhan <kevin.w.zhan@gmail.com>
2015-06-11 16:23:13 -0400

    Edited Renderer

--- src/Renderer.java
+++ src/Renderer.java
@@ -1,10 +1,12 @@
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JComponent;
-
 
 public class Renderer extends JComponent {
 	public Level level;
@@ -15,13 +17,49 @@ public class Renderer extends JComponent {
 
 	public void paintComponent(Graphics g) {
 		Rectangle2D bounds = level.getBounds();
-		BufferedImage canvas = new BufferedImage((int) bounds.getWidth(), (int) bounds.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		BufferedImage canvas = new BufferedImage((int) bounds.getWidth(),
+				(int) bounds.getHeight(), BufferedImage.TYPE_INT_ARGB);
 		level.render(canvas.getGraphics());
-		double scaleFactor = (double) getHeight() / canvas.getHeight();
-//		Image scaled = canvas.getScaledInstance((int) (canvas.getWidth() * scaleFactor), (int) (canvas.getHeight() * scaleFactor), BufferedImage.SCALE_FAST);
-		g.drawImage(canvas, 0, 0, this);
+		double aspectRatio = (canvas.getWidth() / canvas.getHeight());
+		double scaleFactor;
+//		if (getHeight() * aspectRatio > getWidth())
+//			scaleFactor = (double) getWidth() / canvas.getWidth();
+//		else
+			scaleFactor = (double) getHeight() / canvas.getHeight();
+		Image scaled = canvas.getScaledInstance(
+				(int) (canvas.getWidth() * scaleFactor),
+				(int) (canvas.getHeight() * scaleFactor),
+				BufferedImage.SCALE_FAST);
+		g.drawImage(scaled, 0, 0, this);
+
+		// Experimental High Quality Scaling. Broken and probably extremely
+		// inefficient.
+		// AffineTransform scaler = new AffineTransform();
+		//
+		// scaler.scale((int) scaleFactor, (int) scaleFactor);
+		// AffineTransformOp scalerMult = new AffineTransformOp(scaler,
+		// AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+		// BufferedImage multiplied = new BufferedImage(
+		// (int) (canvas.getWidth() * (int) scaleFactor),
+		// (int) (canvas.getHeight() * (int) scaleFactor),
+		// BufferedImage.TYPE_INT_RGB);
+		// scalerMult.filter(canvas, multiplied);
+		//
+		// scaler.scale((int) (canvas.getWidth() * (scaleFactor) / multiplied
+		// .getWidth()),
+		// (int) (canvas.getHeight() * scaleFactor / multiplied
+		// .getHeight()));
+		// AffineTransformOp scalerLine = new AffineTransformOp(scaler,
+		// AffineTransformOp.TYPE_BICUBIC);
+		// BufferedImage scaled = new BufferedImage(
+		// (int) (canvas.getWidth() * scaleFactor),
+		// (int) (canvas.getHeight() * scaleFactor),
+		// BufferedImage.TYPE_INT_RGB);
+		// scalerLine.filter(multiplied, scaled);
+		//
+		// g.drawImage(scaled, 0, 0, this);
 	}
-	
+
 	public void setLevel(Level level) {
 		this.level = level;
 	}
